b1e57fd78c7824955bd15ee82c217a9745e6a8e2
Jo59Devs <110226479+Jo59Devs@users.noreply.github.com>
2025-01-03 13:04:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 # Check if the host directory already contains files
 if [ ! -f /tflite/reiher.py ]; then
     echo "Initialize host directory with files from container..."
-    mv /tmp/reiher.py /tflite/reiher.py
+    mv /tmp/heron.py /tflite/heron.py
 else
     echo "Host directory is already initialized."
 fi
 # Start the main process
-echo "Start the main process: reiher.py"
-exec python3 /tflite/reiher.py
+echo "Start the main process: heron.py"
+exec python3 /tflite/heron.py
